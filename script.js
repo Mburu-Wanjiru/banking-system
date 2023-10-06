b815912ff1d14a7ffd8fb3efa3e00ec2774eb9ff
James Mburu <mburuwanjiru364@gmail.com>
2023-10-06 13:26:10 +0300

    06/1012023

--- script.js
+++ script.js
@@ -6,28 +6,28 @@
 
 // Data
 const account1 = {
-  owner: 'Jonas Schmedtmann',
+  owner: 'James Mburu',
   movements: [200, 450, -400, 3000, -650, -130, 70, 1300],
   interestRate: 1.2, // %
   pin: 1111,
 };
 
 const account2 = {
-  owner: 'Jessica Davis',
+  owner: 'Moses Mbugua',
   movements: [5000, 3400, -150, -790, -3210, -1000, 8500, -30],
   interestRate: 1.5,
   pin: 2222,
 };
 
 const account3 = {
-  owner: 'Steven Thomas Williams',
+  owner: 'Grace Wamaitha',
   movements: [200, -200, 340, -300, -20, 50, 400, -460],
   interestRate: 0.7,
   pin: 3333,
 };
 
 const account4 = {
-  owner: 'Sarah Smith',
+  owner: 'Peter Kimani',
   movements: [430, 1000, 700, 50, 90],
   interestRate: 1,
   pin: 4444,
@@ -68,7 +68,7 @@ const inputClosePin = document.querySelector('.form__input--pin');
 const currencies = new Map([
   ['USD', 'United States dollar'],
   ['EUR', 'Euro'],
-  ['GBP', 'Pound sterling'],
+  [ 'GBP', 'Pound sterling'],
 ]);
 
 // const movements = [200, 450, -400, 3000, -650, -130, 70, 1300];
@@ -121,9 +121,9 @@ const movements = [200, 450, -400, 3000, -650, -130, 70, 1300];
 //use of for of loop
 for (const movement of movements){
   if(movement > 0){
-    console.log(`You deposited Ksh ${movement}`);
+    console.log(`You deposited Ksh ${movement}.`);
   }else{
-    console.log(`You withdraw Ksh ${Math.abs(movement)}`);
+    console.log(`You withdraw Ksh ${Math.abs(movement)}.`);
   }
 }
 
@@ -133,6 +133,50 @@ movements.forEach(function(movement){
   if(movement > 0){
     console.log(`You deposited Ksh ${movement}`);
   }else{
-    console.log(`You withdraw Ksh ${Math.abs(movement)}`);
+    console.log(`You withdrew Ksh ${Math.abs(movement)}`);
+  }
+  // 0:function(200)
+  // 1:function(450)
+});
+
+
+//accessing index using fo of loop
+// :output:Movement 1:You deposited 200
+for(const [a,b] of movements.entries()){
+  if(b >0){
+    console.log(`Movement ${a+1}:You deposited ${b}:::`)
+  }else{
+    console.log(`Movement ${a+1}:You Withdrew ${Math.abs(b)}:::`);
+  }
+}
+
+//access
+//use of for each method.
+movements.forEach(function(mov,i,arr){
+  if(mov>0){
+    console.log(`Movement ${i+1}: You deposited Ksh ${mov}.`)
+  }else{
+    console.log(`Movement ${i+1}: You withdrew Ksh ${Math.abs(mov)}.`)
   }
 })
+
+//FOR EACH in Maps
+
+const moneyMap = new Map([
+  ['USD', 'United States dollar'],
+  ['EUR', 'Euro'],
+  [ 'GBP', 'Pound sterling'],
+]);
+ moneyMap.forEach(function(value,key,map){
+  console.log(`key: ${key} value: ${value}`);
+ })
+
+const moneySet=new Set(['Kenyan shillling','United States Dollar','South African Rand','United States Dollar','Israeli new shekel','United States Dollar','Mexican Peso','Israeli new shekel','Swedish Krona','South African Rand'])
+moneySet.forEach(function(value,index,set){
+  console.log(`value=${value}`)
+})
+
+
+
+
+
