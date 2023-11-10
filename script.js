f2fe4a2df83ba4a4388a0c675f6663a908c26ed9
James Mburu <mburuwanjiru364@gmail.com>
2023-11-10 22:50:01 +0300

    10-11-2023

--- script.js
+++ script.js
@@ -1,4 +1,52 @@
 'use strict';
+/* // Destructuring Objects
+restaurant.orderDelivery({
+  time: '22:30',
+  address: 'Via del Sole, 21',
+  mainIndex: 2,
+  starterIndex: 2,
+});
+
+restaurant.orderDelivery({
+  address: 'Via del Sole, 21',
+  starterIndex: 1,
+});
+
+const { name, openingHours, categories } = restaurant;
+console.log(name, openingHours, categories);
+
+const {
+  name: restaurantName,
+  openingHours: hours,
+  categories: tags,
+} = restaurant;
+console.log(restaurantName, hours, tags);
+
+// Default values
+const { menu = [], starterMenu: starters = [] } = restaurant;
+console.log(menu, starters);
+
+// Mutating variables
+let a = 111;
+let b = 999;
+const obj = { a: 23, b: 7, c: 14 };
+({ a, b } = obj);
+console.log(a, b);
+
+// Nested objects
+const {
+  fri: { open: o, close: c },
+} = openingHours;
+console.log(o, c);
+
+
+///////////////////////////////////////
+// Destructuring Arrays
+const arr = [2, 3, 4];
+const a = arr[0];
+const b = arr[1];
+const c = arr[2];
+ */ 
 // BANKIST APP
 
 // Fetched Data
@@ -302,11 +350,12 @@ console.log(iteration2);
  }
  console.log(newLine); 
 
- ////husling an array 
+ ////husling an array using Map
 
  const strLine=movements.map((mov,i,arr)=>`Movement ${i+1}:
   you ${mov>0?'deposited':'withdrew'} ${Math.abs(mov)}`);
   console.log(strLine);
+   
 
 
  
